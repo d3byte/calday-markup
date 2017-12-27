21acff644563916bd642707e637c3b9ffd0bdc50
Pandinator <cheloveckov.ru@yandex.ru>
2017-12-27 16:05:07 +0300

    сделал ток синий уголок

---
+++ js/onscroll.js
@@ -0,0 +1,20 @@
+// if (isEndOfElement(document.body)) {
+//     document.querySelector('.blue-corner').classList.add('visible')
+// }
+
+// window.onscroll = function (event) {
+//     if (isEndOfElement()) {
+//         console.log('kek')
+//         document.querySelector('.blue-corner').classList.add('visible')
+//     } else {
+//         console.log('kek')
+//         document.querySelector('.blue-corner').classList.remove('visible')
+//     }
+// };
+
+// function isEndOfElement(element = document.body) {
+//     console.log('element.offsetHeight', element.offsetHeight)
+//     console.log('element.scrollTop', element.scrollTop)
+//     console.log('element.scrollHeight', element.scrollHeight)
+//     return element.offsetHeight + element.scrollTop >= element.scrollHeight
+// }
